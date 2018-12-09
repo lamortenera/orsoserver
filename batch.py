9da40b94a840d2ac29195b9ce5893ef0b3ed8b2e
Alessandro Mammana <ale.fener@gmail.com>
2018-12-09 14:28:55 +0100

    Video rendering and plotly graphics

--- batch.py
+++ batch.py
@@ -2,33 +2,69 @@ import os
 import datetime
 import re
 import csv
+import pandas as pd
+import numpy as np
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
+
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 STATIC_DIR = os.path.join(os.path.dirname(__file__), 'static')
 DATA_PATH = os.path.join(STATIC_DIR, 'data.csv')
 TAGS = {
-  "FB:B7:8B:F3:32:53": "A: Camera da letto",
-  "F0:C9:25:26:3B:12": "B: Balcone",
-  "FC:52:08:88:81:E1": "C: Salotto"
+  "FB:B7:8B:F3:32:53": {
+        "name": "A: Camera da letto",
+        "temp_offset": -0.065709},
+  "F0:C9:25:26:3B:12": {
+      "name": "B: Salotto",
+      "temp_offset":  0.065785 },
+  "FC:52:08:88:81:E1": {
+      "name": "C: Balcone",
+      "temp_offset": -0.000077}
 }
 
 COL_TAG_NAME = "tag_name"
+COL_TAG_MAC = "mac"
 COL_TEMP = "temp"
 COL_HUMIDITY = "humidity"
 COL_TIME = "time"
+
+_FONTS_DIR = "/home/pi/Desktop/orsoserver/fonts/"
+_font = ImageFont.truetype(os.path.join(_FONTS_DIR, 'Roboto-Regular.ttf'), size=100)
 
 def read_tags():
   raw_tag_datas = RuuviTagSensor.get_data_for_sensors(TAGS.keys(), 5)
   tag_datas = []
   for mac, raw_tag_data in raw_tag_datas.items():
-    tag_name = TAGS.get(mac)
-    if not tag_name:
+    tag = TAGS.get(mac)
+    if not tag:
       continue
     tag_datas.append({
-        COL_TAG_NAME: tag_name,
+        COL_TAG_MAC: mac,
         COL_TEMP: raw_tag_data.get("temperature"),
         COL_HUMIDITY: raw_tag_data.get("humidity")})
   return tag_datas
+
+def photo_relpath(time_str):
+  return 'photo_%s.jpg' % time_str
+
+def capture_photo(path):
+  os.system('raspistill -n -q 10 -vf -hf -o %s' % path)
+
+def label_photo(input_path, text, output_path):
+  img = Image.open(input_path)
+  draw = ImageDraw.Draw(img)
+  draw.text((50, 30), text, (255, 255, 255), font=_font)
+  img.save(output_path)
+
+def make_video(glob, output_path):
+  #ffmpeg -y -r 20 -pattern_type glob -i 'static/mod*.jpg' -c:v libx264  -pix_fmt yuv420p -s 800x600 -b:v 1M -bufsize 1M static/video2.mp4
+  dirname, basename = os.path.split(output_path)
+  tmp_path = os.path.join(dirname, 'tmp_' + basename)
+  os.system(("ffmpeg -y -r 20 -pattern_type glob -i '{}' -c:v libx264 "
+             "-pix_fmt yuv420p -s 800x600 -b:v 1M -bufsize 1M {}; "
+             " mv -f {} {}").format(glob, tmp_path, tmp_path, output_path))
 
 def read_data():
   try:
@@ -40,6 +76,23 @@ def read_data():
   except:
     pass
   return [], []
+
+def read_dataframe():
+  _, data = read_data()
+  if not data:
+    return None
+  df = pd.DataFrame.from_records(data)
+  df['datetime'] = [str_to_datetime(s) for s in df[COL_TIME]]
+  df[COL_TEMP] = np.array(df[COL_TEMP], dtype=float)
+  df[COL_HUMIDITY] = np.array(df[COL_HUMIDITY], dtype=float)
+  df[COL_TAG_NAME] = "unknown tag"
+  for mac, tag in TAGS.items():
+    rows = df[COL_TAG_MAC] == mac
+    if 'temp_offset' in tag:
+      df.loc[rows, COL_TEMP] -= tag['temp_offset']
+    if 'name' in tag:
+      df.loc[rows, COL_TAG_NAME] = tag['name']
+  return df
 
 def add_to_data(rows):
   fields, data = read_data()
@@ -65,6 +118,13 @@ def run():
   dt = datetime.datetime.now()
   time_str = datetime_to_str(dt)
   tag_datas = read_tags()
+  photo = photo_relpath(time_str)
+  photo_path = os.path.join(STATIC_DIR, photo)
+  capture_photo(photo_path)
+  mod_photo_path = os.path.join(STATIC_DIR, 'mod_' + photo)
+  label_photo(photo_path, dt.strftime("%Y-%m-%d %H:%M:%S"), mod_photo_path)
+  make_video(os.path.join(STATIC_DIR, 'mod*.jpg'), os.path.join(STATIC_DIR,
+                                                                'video.mp4'))
   for data in tag_datas:
     data[COL_TIME] = time_str
   add_to_data(tag_datas)
--- main.py
+++ main.py
@@ -8,43 +8,84 @@ import numpy as np
 import pandas as pd
 import datetime
 import base64
+import json
 from io import BytesIO
 
 app = flask.Flask(__name__)
+_PLOTLY_COLORS = [
+  '#1f77b4',
+  '#ff7f0e',
+  '#2ca02c',
+  '#d62728',
+  '#9467bd',
+  '#8c564b',
+  '#e377c2',
+  '#7f7f7f',
+  '#bcbd22',
+  '#17becf'
+]
 
-def plot_col(df, col, title):
+def to_json(df, col, title, chart_id):
   y = np.array(df[col].replace("", None), dtype=float)
   labels = sorted(df[batch.COL_TAG_NAME].unique())
+  data = []
   for label in labels:
     rows = df[batch.COL_TAG_NAME] == label
-    sub_x = df.x[rows]
+    sub_x = [str(dt) for dt in df.datetime[rows]]
     sub_y = y[rows]
-    plt.plot(sub_x, sub_y, label=label)
-  plt.title(title)
-  plt.legend()
-  figfile = BytesIO()
-  plt.gcf().autofmt_xdate()
-  plt.savefig(figfile, format='png')
-  plt.clf()
-  return base64.b64encode(figfile.getvalue()).decode('ascii')
+    data.append({'x': list(sub_x), 'y': list(sub_y), 'mode': 'lines', 'name':
+                 label})
+  return {'id': chart_id, 'data': data, 'layout': {'title': title}}
 
+def combine_plots(plot1, plot2, title, chart_id):
+  data = []
+  ytitle1 = plot1['layout']['title']
+  legends = {}
+
+  def config_legend(trace):
+    name = trace['name']
+    trace['legendgroup'] = name
+    if name in legends:
+      trace['showlegend'] = False
+    else:
+      legends[name] = _PLOTLY_COLORS[len(legends) % len(_PLOTLY_COLORS)]
+      trace['showlegend'] = True
+    trace['line'] = {'color': legends[name]}
+
+  for trace in plot1['data']:
+    trace['yaxis'] = 'y'
+    config_legend(trace)
+    data.append(trace)
+  ytitle2 = plot2['layout']['title']
+  for trace in plot2['data']:
+    trace['yaxis'] = 'y2'
+    config_legend(trace)
+    data.append(trace)
+
+  grid = {'row': 2, 'columns': 1, 'subplots': [['xy'], ['xy2']]}
+  layout = {'title': title,
+            'grid': grid,
+            'yaxis': {'title': ytitle1, 'domain': [0.52, 1]},
+            'yaxis2': {'title': ytitle2, 'domain': [0, 0.47]}}
+  return {'id': chart_id, 'data': data, 'layout': layout}
 
 @app.route('/')
 def index():
   _, data = batch.read_data()
   time_str = None
-  temp_chart = None
-  humid_chart = None
+  carts_json = None
+  video = flask.url_for('static', filename='video.mp4')
   if data:
-    df = pd.DataFrame.from_records(data)
-    df['x'] = [batch.str_to_datetime(s) for s in df[batch.COL_TIME]]
-    last_time = df.x.iloc[-1]
-    temp_chart = plot_col(df, batch.COL_TEMP, 'temperatura')
-    humid_chart = plot_col(df, batch.COL_HUMIDITY, 'umidità')
+    df = batch.read_dataframe()
+    last_time = df.datetime.iloc[-1]
+    temp_json = to_json(df, batch.COL_TEMP, 'temperatura', 'temp')
+    humid_json = to_json(df, batch.COL_HUMIDITY, 'umidità', 'humid')
+    sensors_json = combine_plots(temp_json, humid_json, 'sensori', 'sensors')
+    charts_json = json.dumps([sensors_json])
   return flask.render_template('index.html',
                                time_str=last_time,
-                               temp_chart=temp_chart,
-                               humid_chart=humid_chart)
+                               charts_json=charts_json,
+                               video=video)
 
 if __name__ == '__main__':
   app.run(debug=True, port=80, host='0.0.0.0')
